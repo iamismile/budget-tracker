b26b5b4c24a587fe3b169f98b2f006010d5d1c7f
Ismile Hossain <iamismile.dev@gmail.com>
2024-05-02 11:57:06 +0600

    authentication setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import Logo from '@/components/Logo';
+import { ReactNode } from 'react';
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div className="relative h-screen flex w-full flex-col items-center justify-center">
+      <Logo />
+      <div className="mt-12">{children}</div>
+    </div>
+  );
+}
+
+export default Layout;
